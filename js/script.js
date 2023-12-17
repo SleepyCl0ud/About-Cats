def49ac037c2894cb7d0f4c25cb56e32cae7693b
Jakub Kozłowski <kuba21970@gmail.com>
2023-12-17 13:06:25 +0100

    js format

--- js/script.js
+++ js/script.js
@@ -3,14 +3,12 @@ let body = document.querySelector(".body");
 let themeName = document.querySelector(".themeName");
 let tableHeader = document.querySelector(".tableHeader");
 
-
 button.addEventListener("click", () => {
-    body.classList.toggle("dark");
-
+  body.classList.toggle("dark");
 });
 
 button.addEventListener("click", () => {
-    tableHeader.classList.toggle("tableHeaderDark");
+  tableHeader.classList.toggle("tableHeaderDark");
 
-    themeName.innerText = body.classList.contains("dark") ? "Jasny" : "Ciemny";
+  themeName.innerText = body.classList.contains("dark") ? "Jasny" : "Ciemny";
 });
